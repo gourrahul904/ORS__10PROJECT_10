d207bfd104063cd5eab4cda1f38908bd4e9059db
gourrahul904 <gourrahul904@gmail.com>
2024-01-31 18:08:47 +0530

    done

---
+++ ORS10/projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * Rahul Gour 
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
